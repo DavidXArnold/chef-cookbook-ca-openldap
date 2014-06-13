89ff6c643edeb7f67210a36621adf0ebc3631b7c
David Arnold <david@newmediadenver.com>
2014-06-13 12:05:56 -0600

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,10 @@
 name             'nmd_openldap'
-#maintainer       'Christophe Arguel'
-#maintainer_email 'christophe.arguel@free.fr'
-#license          'Apache 2.0'
-#description      'Configures a node to be an OpenLDAP server or client.'
+maintainer 'NewMedia! Denver'
+maintainer_email 'support@newmediadenver.com'
+license 'Apache 2.0'
+description      'Configures a node to be an OpenLDAP server or client.'
 #long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-#version          '0.2.1'
+version          '0.3.1'
 
 supports 'redhat', '>= 6.0'
 supports 'centos', '>= 6.0'
-
